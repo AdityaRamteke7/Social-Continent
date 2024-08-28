5b3f8b61a4bdcbaaff6629fd8216baa808eefb15
AdityaRamteke7 <adityaramteke1999@gmail.com>
2024-08-28 17:43:25 +0530

    added Skeleton effect

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -5,12 +5,31 @@ import Post from "./Post";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
+const PostSkeleton = () => (
+  <div className="post-skeleton">
+    <div className="post-header-skeleton">
+      <Skeleton circle={true} height={40} width={40} />
+      <div style={{ marginLeft: "10px", flex: 1 }}>
+        <Skeleton width={100} />
+        <Skeleton width={60} />
+      </div>
+    </div>
+    <div className="post-media-skeleton">
+      <Skeleton height={200} />
+    </div>
+    <div className="post-footer-skeleton">
+      <Skeleton width={80} height={20} />
+      <Skeleton width={80} height={20} style={{ marginLeft: "10px" }} />
+      <Skeleton width={80} height={20} style={{ marginLeft: "10px" }} />
+    </div>
+  </div>
+);
+
 const PostList = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.post.posts);
   const status = useSelector((state) => state.post.status);
 
-  console.log(posts, status);
   useEffect(() => {
     if (status === "idle") {
       dispatch(fetchPosts());
@@ -19,14 +38,12 @@ const PostList = () => {
 
   return (
     <div className="timeline">
-      {status === "loading" ? (
-        <>
-          <Skeleton animation="wave" width={30} height={30} />
-          <Skeleton animation="wave" width={"100%"} height={20} />
-        </>
-      ) : (
-        posts.map((post) => <Post key={post.hash} post={post} />)
-      )}
+      {status === "loading"
+        ? // Render multiple skeletons to simulate a loading list of posts
+          Array.from({ length: 5 }).map((_, index) => (
+            <PostSkeleton key={index} />
+          ))
+        : posts.map((post) => <Post key={post.hash} post={post} />)}
     </div>
   );
 };
